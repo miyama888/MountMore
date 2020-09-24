ad97100d645821ed662fae56e1e37356ded9d541
vagrant <vagrant@localhost.localdomain>
2020-09-24 11:36:31 +0000

    background-image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    # binding.pry
     if @user.update(user_params)
       redirect_to user_path, notice: "You have updated user successfully."
     else
